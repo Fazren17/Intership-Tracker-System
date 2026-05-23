fe45eee2606275069c69ff4a11c912b9daf5c5ef
Fazren Idham <2024230952@student.uitm.edu.my>
2026-05-24 04:21:41 +0800

    Add Internship Tracker System files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function login() {
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if(username === 'admin' && password === '1234') {
+
+        alert('Login Successful!');
+
+        window.location.href = 'dashboard.html';
+
+    } else {
+
+        document.getElementById('error').innerHTML = 'Invalid username or password';
+
+    }
+}
